4d0e6079145507767376271f7634c1946eaf2c6f
Jonatas Tonin Coelho <51675838+JonatasTCoelho@users.noreply.github.com>
2022-03-31 20:56:29 -0300

    first commit

---
+++ aula2/pratica_lista_tupla.py
@@ -0,0 +1,101 @@
+import pickle
+
+
+if __name__ == "__main__":
+    with open('registros.bin', 'rb') as list_in_file:
+        registros = pickle.load(list_in_file)
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+        # for registro in registros:
+        #  if registro[0][0].startswith("M"):
+        #      print(registro[0])
+
+#         for registro in registros:
+#          if registro[1].endswith('SP'):
+#              print(registro)
+
+# estado_SP = []
+# >>> for registro in registros:
+# ...     if registro[1].endswith('SP'):
+# ...             estado_SP.append(registro)
+
+=======
+=======
+>>>>>>> efc8cc9b23ff3af34fb3c81ad3e71fa92f239f5f
+
+# Desafio 1: Capturar os nomes que começam com a letra "M"
+# Solução 1
+
+for registro in registros:
+    if registro[0][0] == 'M':
+        print(registro[0])
+
+for registro in registros:
+    if registro[0].startswith('M'):
+        print(registro[0])
+
+# Desafio 2: Capturar os registros com endereços do estado de SP
+# e armazenar em uma lista chamada "estado_SP"
+
+estado_SP = []
+for registro in registros: 
+    if registro[1][-2:] == 'SP': 
+        estado_SP.append(registro) 
+
+estado_SP = []    
+for registro in registros: 
+    if '/ SP' in registro[1]: 
+        estado_SP.append(registro) 
+    
+estado_SP = []
+for registro in registros: 
+    if registro[1].endswith('SP'): 
+        estado_SP.append(registro) 
+
+# Desafio 3: Remover itens duplicados de uma lista
+
+lista_mercado = ['Arroz', 'Feijão', 'Macarrão', 'Coca-cola']
+lista_mercado += ['Arroz', 'Coca-cola', 'arroz', 'coca-cola']
+lista_mercado += ['Arroz', 'Feijão', 'Macarrão', 'Coca-cola']
+lista_mercado += ['Arroz', 'Coca-cola', 'arroz', 'coca-cola']
+
+lista_mercado = list(set(lista_mercado))
+
+numeros = set(range(1,10)) 
+# {1, 2, 3, 4, 5, 6, 7, 8, 9}
+numeros2 = set(range(5,15))
+# {5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+numeros2.intersection(numeros) 
+# {5, 6, 7, 8, 9}
+numeros.union(numeros2)
+# {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+numeros.difference(numeros2) 
+# {1, 2, 3, 4}
+numeros.symmetric_difference(numeros2)  
+# {1, 2, 3, 4, 10, 11, 12, 13, 14}
+<<<<<<< HEAD
+>>>>>>> be91e1a8b120331bba89c8bd188bc7575ce810f7
+=======
+
+# Dicionário
+a = dict(one=1, two=2, three=3)
+b = {'one': 1, 'two': 2, 'three': 3}
+ = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+d = dict([('two', 2), ('one', 1), ('three', 3)])
+e = dict({'three': 3, 'one': 1, 'two': 2})
+f = dict({'one': 1, 'three': 3}, two=2)
+print(a == b == c == d == e == f)
+# True
+
+lista_telefonica = {} 
+lista_telefonica['Orlando'] = ('Orlando','99999-8888')  
+lista_telefonica['Fatec'] = ['Faculdade',15,'99999-8888')
+
+lista_telefonica.get('orlando', 'Não Encontrado')
+# Não Encontrado
+lista_telefonica.get('Orlando', 'Não Encontrado')
+# ('Orlando', '99999-8888')
+
+>>>>>>> efc8cc9b23ff3af34fb3c81ad3e71fa92f239f5f
+
---
+++ pratica_OO/pratica_OO.py
@@ -0,0 +1,95 @@
+from datetime import date
+
+class Aluno:
+    def __init__(self, nome, sobrenome, curso):
+            self._nome = nome
+            self._sobrenome = sobrenome
+            self._curso = curso
+
+            
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, valor):
+        self._nome = valor
+
+
+    @property
+    def sobrenome(self):
+        return self._sobrenome
+
+    @sobrenome.setter
+    def sobrenome(self, valor):
+        self._sobrenome = valor
+
+    def __str__(self):
+        return self._nome
+
+    def __repr__(self):
+         return self._nome + ' ' + self._sobrenome
+
+
+class Professor:
+    def __init__(self, nome, sobrenome):
+            self._nome = nome
+            self._sobrenome = sobrenome
+            self._disciplinas = []
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, valor):
+        self._nome = valor
+
+    @property
+    def disciplina(self):
+         return self._disciplinas
+    
+
+    def adicionar_disciplina(self, disciplina):
+         self._disciplinas.append(disciplina)
+
+    def __str__(self):
+        return self._nome
+
+    def __repr__(self):
+         return self._nome
+
+
+class SalaAula:
+    def __init__(self, professor):
+        if isinstance(professor, Professor):
+            self._professor = professor
+        else:
+            raise TypeError('Tipo do professor precisa instância de Professor')
+        self._alunos = []
+
+    @property
+    def professor(self):
+        return str(self._professor)
+
+    @property
+    def alunos(self):
+        return self._alunos
+
+    def adicionar_aluno(self, nome, sobrenome, curso):
+        novoAluno = Aluno(nome, sobrenome, curso)
+        self._alunos.append(novoAluno)
+
+    def busca_aluno(self, nome_procurado):
+
+             for aluno in self._alunos:
+                 if aluno.nome == nome_procurado:
+                    return aluno
+             return None       
+
+    def __str__(self):
+        return 'Sala com prof. ' + self.professor
+
+    def __repr__(self):
+        return str(self)
+
